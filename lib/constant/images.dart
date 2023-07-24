02bed49ea10eced409a05735888b1c54c6b9413b
crazyads69 <leminhtri11a1@gmail.com>
2023-07-24 13:06:34 +0700

    Define image and icon path

---
+++ lib/constant/images.dart
@@ -0,0 +1,12 @@
+//Define Image Path
+const albumImage = 'assets/images/Album_1.svg';
+const albumImage_1 = 'assets/images/Album_2.svg';
+const artistImage = 'assets/images/Artist_1.svg';
+const artistImage_1 = 'assets/images/Artist_2.svg';
+const artistPageImage = 'assets/images/ArtistPage.svg';
+const bannerImage = 'assets/images/Banner.svg';
+const musicPageImage = 'assets/images/MusicPage.svg';
+const playlistImage = 'assets/images/Playlist_1.svg';
+const playlistImage_1 = 'assets/images/Playlist_2.svg';
+const songImage = 'assets/images/Song_1.svg';
+const songImage_1 = 'assets/images/Song_2.svg';
