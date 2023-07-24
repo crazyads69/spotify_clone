10c7a5177178cb5eef4314ea8a2e68c0ffbff431
crazyads69 <leminhtri11a1@gmail.com>
2023-07-24 15:00:39 +0700

    Add splash screen and launcher icon

--- lib/constant/images.dart
+++ lib/constant/images.dart
@@ -10,3 +10,4 @@ const playlistImage = 'assets/images/Playlist_1.svg';
 const playlistImage_1 = 'assets/images/Playlist_2.svg';
 const songImage = 'assets/images/Song_1.svg';
 const songImage_1 = 'assets/images/Song_2.svg';
+const welcomeBackgroundImage = 'assets/images/WelcomeBackground.svg';
